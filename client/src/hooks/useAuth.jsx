8479f552a2a22ec6cc9d141c4e94792a0e9af209
EGARRISXN <egarrisxn@gmail.com>
2024-09-01 13:16:09 -0400

    little tweak to package.json

--- client/src/hooks/useAuth.jsx
+++ client/src/hooks/useAuth.jsx
@@ -1,4 +1,3 @@
-/* eslint-disable react-refresh/only-export-components */
 import { createContext, useContext, useMemo } from "react";
 import { useNavigate } from "react-router-dom";
 import { useLocalStorage } from "./useLocalStorage";
